2c4e069bdd1d5cf8e32ac819480a86d01b8d2f1e
anxiongbo <anxiongbo@zhangyue.com>
2015-10-29 17:54:59 +0800

    增加修改

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -4,4 +4,5 @@ import os.path
 STATIC_PATH   = os.path.join(os.path.dirname(__file__), "../static")
 TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), "../templates")
 HANDLERS =[(r"/" ,Index_Handler),
+           (r"/xen_operation/",Xen_operation_Handler),
 	]
--- views/views.py
+++ views/views.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.6
 # -*- coding: utf-8 -*- 
 import tornado.web
+import salt.client
 import redis
 class BaseHandler(tornado.web.RequestHandler):
     @property
@@ -11,3 +12,27 @@ class Index_Handler(BaseHandler):
 	r=redis.Redis(host='localhost', port=6386)
 	Xen_Data = r.hgetall("push_xenserver")
         self.render('index.html', Xen_Data= Xen_Data) 
+
+
+
+
+
+
+#xe vm-start   vm=1c12db36-7ec3-52ba-6360-5b60c0418534
+#xe vm-shutdown  vm=1c12db36-7ec3-52ba-6360-5b60c0418534
+#xe vm-reboot   vm=1c12db36-7ec3-52ba-6360-5b60c0418534
+
+class Xen_operation_Handler(BaseHandler):
+    '''
+    获取xen的uuid和IP
+    '''
+    def post(self):
+        uuid = self.get_argument("uuid")
+        xen_ip = self.get_argument("xen_ip")
+        action = self.get_argument("action")
+        print self.request.arguments
+        local = salt.client.LocalClient()
+        result_json = local.cmd_async(xen_ip , 'cmd.run', ['xe vm-%s   vm=%s'%(action,uuid)])
+        print result_json
+
+        self.write(self.request.arguments)
